fef00870e22f0e37c969b40c702edcf914bc4419
orewaminhdesu <minhhnim134@gmail.com>
2026-08-19 23:11:10 +0700

    First commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    public float currentMoveSpeed;
+    [SerializeField] private float rotationSpeed = 10f;
+    [SerializeField] private Rigidbody rb;
+
+    private Vector3 moveDirection;
+    
+    [SerializeField] private Animator animator;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (rb == null) rb = GetComponent<Rigidbody>();
+        if (animator == null) animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+        RotatePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        moveDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+
+        rb.MovePosition(rb.position + moveDirection * currentMoveSpeed * Time.fixedDeltaTime);
+        currentMoveSpeed = moveDirection.magnitude * moveSpeed;
+        animator.SetFloat("Blend", currentMoveSpeed);
+    }
+
+    private void RotatePlayer()
+    {
+        if (moveDirection != Vector3.zero)
+        {
+            Quaternion lookDirection = Quaternion.LookRotation(moveDirection);
+            rb.MoveRotation(
+                Quaternion.Slerp(
+                    rb.rotation,
+                    lookDirection,
+                    rotationSpeed * Time.fixedDeltaTime
+                    ));
+        }else return;
+    }
+}
